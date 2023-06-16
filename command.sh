6b33c7e0cf8f09b173b9fa1534aab75452a5ef8a
tuyennn3 <casterpin2@gmail.com>
2023-06-16 02:33:12 +0000

    fix command

--- command.sh
+++ command.sh
@@ -1,1 +1,5 @@
-git clone git@github.com:casterpin2/udacity-lab-2-1.git cd udacity-lab-2-1 git pull make all az webapp up -n flask-app-tuyennn3 --resource-group tuyennn3_rg_0674 --sku FREE
+git clone git@github.com:casterpin2/udacity-lab-2-1.git 
+cd udacity-lab-2-1 
+git pull 
+make all 
+az webapp up -n flask-app-tuyennn3 --resource-group tuyennn3_rg_0674 --sku FREE
